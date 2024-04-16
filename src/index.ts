66b40a88b976325a24857f89b2ed2f7b9e0e130b
kvasileva <kvasileva@megaputer.ru>
2024-04-16 20:31:14 +0300

    Call dram method from index.htm and prepare for string array

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,74 @@
 import * as d3 from "d3";
 
-const data: number[] = [1, 2, 2, 2, 3, 3, 4, 5];
-// set the dimensions and margins of the graph
-const margin = { top: 10, right: 30, bottom: 30, left: 40 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+declare global {
+    interface Array<T> {
+        draw(): void;
+    }
+}
 
-// append the svg object to the body of the page
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        `translate(${margin.left},${margin.top})`);
+function getXScale<T extends (number | string)[]>(data: T, boxWidth: number){
+    if (typeof data[0] === "number"){
+        return d3.scaleBand<number>()
+            .domain(data.map((d, i) => i))
+            .range([0, boxWidth])
+            .paddingInner(0.05)
+            .paddingOuter(0.01);
+    }
+    if (typeof data[0] === "string"){
+        return d3.scaleLinear()
+            .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+            .range([0, width]);
+    }
+}
 
-// X axis: scale and draw:
-const x = d3.scaleBand<number>()
-    .domain(data.map((d, i) => i))
-    .range([0, width])
-    .paddingInner(0.05);
+Array.prototype.draw = function<T extends (number | string)>(this: T[]) {
+    // set the dimensions and margins of the graph
+    const margin = { top: 10, right: 30, bottom: 30, left: 40 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
 
-const y = d3.scaleLinear()
-    .domain([0, d3.max(data)!])
-    .range([height, 0]);
+    // append the svg object to the body of the page
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            `translate(${margin.left},${margin.top})`);
 
-const [min, max] = d3.extent(data);
-const color = d3.scaleLinear<string, number>()
-    .domain([min!, max!])
-    .range(['yellow', 'red']);
+    // X axis: scale and draw:
+    const x = d3.scaleBand<number>()
+        .domain(this.map((d, i) => i))
+        .range([0, width])
+        .paddingInner(0.05)
+        .paddingOuter(0.01);
 
-svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x));
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(this)!])
+        .range([height, 0]);
 
-svg.append("g")
-    .call(d3.axisLeft(y));
+    const [min, max] = d3.extent(this);
+    const color = d3.scaleLinear<string, number>()
+        .domain([min!, max!])
+        .range(['yellow', 'red']);
 
-// append the bar rectangles to the svg element
-svg.selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("x", i => x(i)!)
-    .attr("y", d => y(d))
-    .attr("width", x.bandwidth())
-    .attr("height", function (d) { return height - y(d); })
-    .attr("fill", d => color(d))
-    .append("title")
-    .text(d => d);
+    svg.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    
+    // append the bar rectangles to the svg element
+    svg.selectAll("rect")
+        .data(this)
+        .join("rect")
+        .attr("x", (d, i) => x(i)!)
+        .attr("y", (d) => y(d))
+        .attr("width", x.bandwidth())
+        .attr("height", function (d, i) { return height - y(d); })
+        .attr("fill", d => color(d))
+        .append("title")
+        .text(d => d);
+}
